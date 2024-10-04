0c6a74d4c57e7a6de2d0110f1903706596a80473
AlessandroPieragostini <S1119377@studenti.univpm.it>
2024-10-05 00:11:53 +0200

    docker-compose funzionante

--- payments_backend/src/config/database.ts
+++ payments_backend/src/config/database.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
+const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS! as string, {
   host: process.env.DB_HOST,
   dialect: 'postgres',
 });
