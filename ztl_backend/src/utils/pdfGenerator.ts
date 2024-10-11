c3a63ad238ba21300b2d52656639735fc9425ec3
AlessandroPieragostini <S1119377@studenti.univpm.it>
2024-10-11 12:18:10 +0200

    generazione bollettino funzionante

--- ztl_backend/src/utils/pdfGenerator.ts
+++ ztl_backend/src/utils/pdfGenerator.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 import Multa from '../models/multa'; // Assicurati che il modello Multa sia correttamente importato
 import { createWriteStream } from 'fs';
 import { v4 as uuidv4 } from 'uuid'; // Per generare UUID
+import QRCode from 'qrcode';
 
 // Funzione per generare il PDF
 export const generatePDF = async (multa: Multa) => {
@@ -20,17 +21,15 @@ export const generatePDF = async (multa: Multa) => {
   doc.fontSize(25).text('Bollettino di Pagamento', { align: 'center' });
   doc.moveDown();
   doc.fontSize(16).text(`Targa: ${multa.targaVeicolo}`, { align: 'left' });
-  doc.text(`Importo: €${multa.importo.toFixed(2)}`, { align: 'left' });
+  doc.text(`Importo: €${multa.importo}`, { align: 'left' });
   doc.text(`ID Multa: ${multa.id}`, { align: 'left' });
-  doc.text(`UUID Pagamento: ${uuidv4()}`, { align: 'left' }); // Genera UUID per il pagamento
+  doc.text(`UUID Pagamento: ${multa.uuidPagamento}`, { align: 'left' }); // Genera UUID per il pagamento
   doc.moveDown();
 
   // Genera un QR Code (puoi usare una libreria esterna per generare il QR code se necessario)
   // Ad esempio, usando `qrcode`:
-  // const qrCode = await QRCode.toDataURL(`uuid pagamento|${multa.id}|${multa.targaVeicolo}|${multa.importo}`);
-  // doc.image(qrCode, { fit: [100, 100], align: 'center' });
-
-  doc.text('Grazie per il pagamento!', { align: 'center' });
+  const qrCode = await QRCode.toDataURL(`${multa.uuidPagamento}|${multa.id}|${multa.targaVeicolo}|${multa.importo}`);
+  doc.image(qrCode, { fit: [100, 100], align: 'center' });
 
   // Finalizza il PDF
   doc.end();
